9e09f81030aea1dde1e09cf765ba6ec35071df92
RaiDenNsg <kcaryan899@gmail.com>
2026-04-12 20:48:42 +0545

    fix: update .gitignore to include .env and refactor Firebase config to use environment variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeXhjubogCTS4cmEu66F6cmLh9Fn9e9xs",
-  authDomain: "mindpulse-a017a.firebaseapp.com",
-  projectId: "mindpulse-a017a",
-  storageBucket: "mindpulse-a017a.firebasestorage.app",
-  messagingSenderId: "948762227393",
-  appId: "1:948762227393:web:cf187395c4c99b5a441a40",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
